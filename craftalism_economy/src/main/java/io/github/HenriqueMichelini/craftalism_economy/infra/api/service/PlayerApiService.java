26f190fba9d0cb6aca8a242460a20660d32e4b33
HenriqueMichelini <henrique.g.michelini@gmail.com>
2025-11-22 00:00:54 -0300

    feat: added PlayerApiService.java

---
+++ craftalism_economy/src/main/java/io/github/HenriqueMichelini/craftalism_economy/infra/api/service/PlayerApiService.java
@@ -0,0 +1,60 @@
+package io.github.HenriqueMichelini.craftalism_economy.infra.api.service;
+
+import com.google.gson.Gson;
+import io.github.HenriqueMichelini.craftalism_economy.infra.api.client.HttpClientService;
+import io.github.HenriqueMichelini.craftalism_economy.infra.api.dto.PlayerResponseDTO;
+import io.github.HenriqueMichelini.craftalism_economy.infra.api.dto.PlayerRequestDTO;
+import io.github.HenriqueMichelini.craftalism_economy.infra.api.exception.NotFoundException;
+
+import java.util.UUID;
+import java.util.concurrent.CompletableFuture;
+
+public class PlayerApiService {
+    private final HttpClientService http;
+    private final Gson gson = new Gson();
+
+    public PlayerApiService(HttpClientService http) {
+        this.http = http;
+    }
+
+    public CompletableFuture<PlayerResponseDTO> getPlayerByUuid(UUID uuid) {
+        return http.get("/players/" + uuid)
+                .thenCompose(resp -> {
+                    if (resp.statusCode() == 404) {
+                        return CompletableFuture.failedFuture(new NotFoundException());
+                    }
+                    return CompletableFuture.completedFuture(
+                            gson.fromJson(resp.body(), PlayerResponseDTO.class)
+                    );
+                });
+    }
+
+    public CompletableFuture<PlayerResponseDTO> getPlayerByName(String name) {
+        return http.get("/players/" + name)
+                .thenCompose(resp -> {
+                    if (resp.statusCode() == 404) {
+                        return CompletableFuture.failedFuture(new NotFoundException());
+                    }
+                    return CompletableFuture.completedFuture(
+                            gson.fromJson(resp.body(), PlayerResponseDTO.class)
+                    );
+                });
+    }
+
+    public CompletableFuture<PlayerResponseDTO> createPlayer(UUID uuid, String name) {
+        PlayerRequestDTO dto = new PlayerRequestDTO(uuid, name);
+
+        return http.post("/players", gson.toJson(dto))
+                .thenApply(response -> gson.fromJson(response.body(), PlayerResponseDTO.class));
+    }
+
+    public CompletableFuture<PlayerResponseDTO> getOrCreatePlayer(UUID uuid, String name) {
+        return getPlayerByUuid(uuid)
+                .exceptionallyCompose(ex -> {
+                    if (ex instanceof NotFoundException) {
+                        return createPlayer(uuid, name);
+                    }
+                    return CompletableFuture.failedFuture(ex);
+                });
+    }
+}
