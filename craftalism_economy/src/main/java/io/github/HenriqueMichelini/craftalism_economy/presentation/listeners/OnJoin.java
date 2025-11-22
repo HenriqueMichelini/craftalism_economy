8094945f90235f9ba2dcd7d18acc09e6c14fa1b7
HenriqueMichelini <henrique.g.michelini@gmail.com>
2025-11-22 00:07:24 -0300

    fix: OnJoin.java

--- craftalism_economy/src/main/java/io/github/HenriqueMichelini/craftalism_economy/presentation/listeners/OnJoin.java
+++ craftalism_economy/src/main/java/io/github/HenriqueMichelini/craftalism_economy/presentation/listeners/OnJoin.java
@@ -1,13 +1,35 @@
 package io.github.HenriqueMichelini.craftalism_economy.presentation.listeners;
 
+import io.github.HenriqueMichelini.craftalism_economy.application.service.PlayerApplicationService;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 
+import java.util.UUID;
+import java.util.logging.Logger;
+
 public class OnJoin implements Listener {
+    private final PlayerApplicationService service;
+    private final Logger logger;
+
+    public OnJoin(PlayerApplicationService service, Logger logger) {
+        this.service = service;
+        this.logger = logger;
+    }
+
     @EventHandler
     public void onPlayerJoin(PlayerJoinEvent event) {
         Player player = event.getPlayer();
+
+        UUID uuid = player.getUniqueId();
+        String name = player.getName();
+
+        service.loadPlayerOnJoin(uuid, name)
+                .exceptionally(ex -> {
+                    logger.warning("Erro ao carregar player " + uuid + ": " + ex);
+                    return null;
+                });
+
     }
 }
