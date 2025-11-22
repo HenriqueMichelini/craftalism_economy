fa46370a73e85a6f64c4ac7e9537ceb953790e3e
HenriqueMichelini <henrique.g.michelini@gmail.com>
2025-11-22 00:06:35 -0300

    feat: added PayResult.java

---
+++ craftalism_economy/src/main/java/io/github/HenriqueMichelini/craftalism_economy/domain/service/enums/PayResult.java
@@ -0,0 +1,10 @@
+package io.github.HenriqueMichelini.craftalism_economy.domain.service.enums;
+
+public enum PayResult {
+    SUCCESS,
+    TARGET_NOT_FOUND,
+    NOT_ENOUGH_FUNDS,
+    INVALID_AMOUNT,
+    CANNOT_PAY_SELF,
+    ERROR
+}
